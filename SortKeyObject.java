07d16a48b7d2769167d18f9841a0c5cd829788ef
shwetatripathi4927 <shwetatripathifeb5@gmail.com>
2021-05-27 17:11:53 +0530

    update in tech test

--- SortKeyObject.java
+++ SortKeyObject.java
@@ -1,20 +1,24 @@
-import java.util.*;
-class sortmaoKey {
-	static Map<Integer, String> obj= new HashMap<>();
-	public static void sortbykey()
-	{
-		TreeMap<Integer, String> sortObj= new TreeMap<>();
-		sortObj.putAll(obj);
-		System.out.print("sorted = ");	
-		for (Map.Entry<Integer, String> entry : sortObj.entrySet())
-			System.out.println(entry.getKey());	
+//program 5
+//sort Dictionary by IDs
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class SortKeyObject{
+	public static void main(String[] args) {
+		Map<Integer, String>obj= new TreeMap<>();
+		obj.put(4,"abc");
+		obj.put(10,"ab2");
+		obj.put(5,"abc3");
+		obj.put(6,"abc5");
+		
+		for(Map.Entry<Integer, String> entry : obj.entrySet() ) {
+			
+			Integer key= entry.getKey();
+			String element = entry.getValue();
+			System.out.println("id: " + key + "\t\tName:" + element);
+		}
+				
 	}
-	public static void main(String args[])
-	{
-		obj.put(4, "abc");
-		obj.put(10, "ab2");
-		obj.put(5, "abc3");
-		obj.put(6, "abc5");
-		sortbykey();
-	}
+
 }
